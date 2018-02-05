45679c49b1635667f46bc699aa778d68f49279a8
melodie <josmelodie@gmail.com>
2018-02-06 00:53:53 +1300

    Added detail action method

---
+++ src/WeddingGallery/Controllers/HarryPotterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingGallery.Controllers
+{
+    public class HarryPotterController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello from the Harry Potter Controller!";
+        }
+    }
+}
