2a561862a87b0408ebceac973a768720bf913f98
melodie <josmelodie@gmail.com>
2018-02-18 23:09:19 +1300

    Updated Wedding Detail view

--- src/WeddingGallery/Controllers/HarryPotterController.cs
+++ src/WeddingGallery/Controllers/HarryPotterController.cs
@@ -10,6 +10,16 @@ namespace WeddingGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.WhatsHappening = "Mel and Art's Harry Potter wedding!";
+            ViewBag.WeddingDate = new DateTime(2019, 3, 9, 16, 0, 0);
+
+            ViewBag.Description = "You are cordially invited to the wedding of Artem Igorevich Shkumat and Melodie Laura Joseph <br /> We will be taking the Lebedev name";
+            ViewBag.Venues = new string[]
+            {
+                "Ceremony: St Patrick's Cathedral",
+                "Reception: TBC"
+            };
+
             return View();
 
         }
